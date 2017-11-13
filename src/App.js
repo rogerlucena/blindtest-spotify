afcd7e8446970841cd619db9f2946244e9325345
Roger Leite Lucena <rogerleitelucena@gmail.com>
2017-11-13 21:13:04 +0100

    Getting songs from Spotify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 /*global swal*/
 
+// token=    BQDGTe2XfR7teWizu1ZwpFv4JAjZ5bYYoV-XXKQZYN6zgW3o5FzL_Ah0UtgXBl_mMTOyngNvUhyeaGhiP64uaws3_yHN1UfSWiHUOM-q0t2EBpxDzLdnBK4bPe3WNAARrfbAlbmY1CIDYCf4MvtCXBEBaPruQqaqZcx_yq92Kc0
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import loading from './loading.svg';
@@ -7,7 +9,7 @@ import './App.css';
 import Sound from 'react-sound';
 import Button from './Button';
 
-const apiToken = '<<Copiez le token de Spotify ici>>';
+const apiToken = 'BQDGTe2XfR7teWizu1ZwpFv4JAjZ5bYYoV-XXKQZYN6zgW3o5FzL_Ah0UtgXBl_mMTOyngNvUhyeaGhiP64uaws3_yHN1UfSWiHUOM-q0t2EBpxDzLdnBK4bPe3WNAARrfbAlbmY1CIDYCf4MvtCXBEBaPruQqaqZcx_yq92Kc0';
 
 function shuffleArray(array) {
   let counter = array.length;
@@ -28,26 +30,88 @@ function getRandomNumber(x) {
   return Math.floor(Math.random() * x);
 }
 
+class AlbumCover extends Component {
+  render() {
+    const src = "https://example.com/image.png"; // A changer ;)
+    return (
+    	<img src={src} style={{ width: 400, height: 400 }} />
+    );
+  }
+}
+
 class App extends Component {
 
   constructor() {
     super();
+    this.state = {
+    	text: "" ,
+    	songsLoaded: false,
+    	firstTrackName: "",
+    	tracks: {}
+    }
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Welcome to the Blindtest</h1>
-        </header>
-        <div className="App-images">
-          <p>Here we go with the Web Dojo !</p>
-        </div>
-        <div className="App-buttons">
-        </div>
-      </div>
-    );
+  	if(this.state.songsLoaded){
+		// <!--To be printing the javascript variable1 you use {}-->
+	    return (
+	      <div className="App">
+	        <header className="App-header">
+	          <img src={logo} className="App-logo" alt="logo"/>
+	          <h1 className="App-title">Welcome to the Blindtest</h1>
+	        </header>
+	        <div className="App-images">
+	        	<AlbumCover/>
+		        <div>{this.state.text} </div>
+		        <div>{this.state.firstTrackName}</div>
+		        <p>Here we go with the Web Dojo !</p>
+
+		        </div>
+		        <div className="App-buttons">
+		        </div>
+	      	</div>
+	    );
+  	} 
+  	else{
+  		return (
+	      <div className="App">
+	        <header className="App-header">
+	          <img src={loading} className="Loading" alt="loading"/>
+	          <h1 className="App-title">Welcome to the Blindtest</h1>
+	        </header>
+	        <div className="App-images">
+
+	        {this.state.text} 
+	          <p>Loading!</p>
+
+	        </div>
+	        <div className="App-buttons">
+	        </div>
+	      </div>
+	    );
+  	}
+  	
+  }
+
+  componentDidMount() {
+  	this.setState({text: "Bonjour"});
+
+  	fetch('https://api.spotify.com/v1/me/tracks', {
+	  method: 'GET',
+	  headers: {
+	   Authorization: 'Bearer ' + apiToken,
+	  },
+	})
+	  .then(response => response.json())
+	  .then((data) => {
+	    console.log("Reponse reçue ! Voilà ce que j'ai reçu : ", data); // F12 no browser para ver o console!
+	  	this.setState({songsLoaded: true});
+	  	this.setState({tracks: data.items});
+	  	this.setState({text: "On a bien reçu tout de Spotify ! =D -- Length: " + this.state.tracks.length});
+	  	this.setState({firstTrackName: "First song: " + data.items[0].track.name});
+
+	  })
+  
   }
 }
 
